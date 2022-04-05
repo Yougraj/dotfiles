e8102be7d9e1d0d984c5d95673dcd77bd0529ce7
Yougraj Bora <84845528+Yougraj@users.noreply.github.com>
2022-04-05 20:33:40 +0530

    Add files via upload

---
+++ bspwm.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+yay -S xorg xorg-xinit bspwm-rounded-corners sxhkd tint2 polybar alacritty xwallpaper maim imagemagick dmenu arandr picom cava ranger
+
+git clone --branch nord https://github.com/Mangeshrex/dotfiles
