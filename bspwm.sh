15f268c91c1cbf7d3630108ed3a5c4934d2ad1be
Yougraj Bora <84845528+Yougraj@users.noreply.github.com>
2022-04-05 21:03:25 +0530

    Update bspwm.sh

--- bspwm.sh
+++ bspwm.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 yay -S xorg xorg-xinit bspwm-rounded-corners sxhkd tint2 polybar alacritty xwallpaper maim imagemagick dmenu arandr picom cava ranger
-
-git clone --branch nord https://github.com/Mangeshrex/dotfiles
