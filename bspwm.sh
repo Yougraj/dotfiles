e92a58b5452f077fb816f10e7c63599242ec26ad
Yougraj Bora <84845528+Yougraj@users.noreply.github.com>
2022-04-07 10:41:43 +0530

    Update bspwm.sh

--- bspwm.sh
+++ bspwm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-yay -S xorg xorg-xinit bspwm-rounded-corners sxhkd tint2 polybar-git alacritty xwallpaper maim imagemagick dmenu arandr picom-git cava ranger
+yay -S xorg xorg-xinit bspwm-rounded-corners networkmanager iw wpa_supplicant dialog sxhkd tint2 polybar-git alacritty xwallpaper maim imagemagick dmenu arandr picom-git cava ranger
